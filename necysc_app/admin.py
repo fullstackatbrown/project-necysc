1f52838d5c35861712faf883839c4108a6fde4ca
Kieran <kieranllamay@gmail.com>
2024-10-15 15:42:58 -0400

    admin landing page updated

--- necysc_app/admin.py
+++ necysc_app/admin.py
@@ -8,10 +8,13 @@ class ApplicantAdmin(admin.ModelAdmin):
 
     # should the timie of app refer to the creation time or time last updated?
     list_display =['applicant_fname', 'applicant_lname', 'applicant_bday', 'last_updated_at', 
-                   'program', 'group_id', 'room_id', 'applicant_sex', 'app_status']
+                   'program', 'group_id', 'room_id', 'applicant_sex', 'app_status', 'payment_received', 
+                   'health_record_status', 'parent1_fname', 'parent1_lname', 'parent1_email', 'parent1_phone', 
+                   'applicant_shirt_size', 'referrals', 'preferred_roommate']
 
     # filter selector based on year
-    list_filter = ['app_year', 'program', 'group_id', 'room_id', 'applicant_sex', 'app_status']
+    list_filter = ['app_year', 'program', 'group_id', 'room_id', 'applicant_sex', 'app_status', 'payment_received',
+                   'health_record_status', 'applicant_shirt_size', 'referrals', 'preferred_roommate']
 
 # Register your models here.
 admin.site.register(Applicant, ApplicantAdmin)
