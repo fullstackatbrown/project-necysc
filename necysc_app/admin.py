64ecd8e4ef1927bcecb9b333c8f595687542dc81
johnlhuangP <john_huang@brown.edu>
2024-11-17 13:34:46 -0500

    feat: Download all applications as csv

--- necysc_app/admin.py
+++ necysc_app/admin.py
@@ -2,8 +2,22 @@
 from django.contrib import admin
 from .models import Applicant, ApplicantHealth, GlobalData
 from solo.admin import SingletonModelAdmin
+from django.http import HttpResponse
+import csv
 
 class ApplicantAdmin(admin.ModelAdmin):
+    def download_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+        writer.writerow(field_names)
+        for obj in queryset:
+            writer.writerow([getattr(obj, field) for field in field_names])
+        return response
+
+    download_csv.short_description = "Download selected applications as CSV"
     search_fields = ['applicant_fname', 'applicant_lname']
     list_display = ['applicant_fname', 'applicant_lname', 'applicant_bday', 'last_updated_at', 
                     'program', 'group_id', 'room_id', 'applicant_sex', 'app_status', 'payment_received', 
@@ -12,6 +26,7 @@ class ApplicantAdmin(admin.ModelAdmin):
     list_filter = ['app_year', 'program', 'group_id', 'room_id', 'applicant_sex', 'app_status', 
                    'payment_received', 'health_record_status', 'applicant_shirt_size', 'referrals', 
                    'preferred_roommate']
+    actions = ["download_csv"]
 
 class HealthInfoAdmin(admin.ModelAdmin):
     search_fields = ['applicant_fname', 'applicant_lname']
